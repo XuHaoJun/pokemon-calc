22fbfe03afeb6b23a2d7f1ee9220f2a067889fdf
noah.hsu <xuhaojuntw@gmail.com>
2024-07-14 02:52:44 +0800

    add lang switcher component

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
 import { t, Trans } from "@lingui/macro";
 import { withLinguiPage } from "@/withLingui";
+import { LangSwitcher } from "@/components/LangSwitcher";
 
 export default withLinguiPage(function Home() {
   return (
     <div>
+      <LangSwitcher />
       <Button>
         <Trans>Click me</Trans>
       </Button>
