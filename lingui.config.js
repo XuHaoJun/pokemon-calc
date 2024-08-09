f9fb522d55cf568a6e2b7facd976e83de00c92b6
xuhaojun <xuhaojuntw@gmail.com>
2024-08-09 17:46:43 +0800

    feat: drop pseudo lang

--- lingui.config.js
+++ lingui.config.js
@@ -1,7 +1,6 @@
 /** @type {import('@lingui/conf').LinguiConfig} */
 module.exports = {
-  locales: ["en", "zh-Hant", "zh-Hans", "ja", "ko", "pseudo"],
-  pseudoLocale: "pseudo",
+  locales: ["en", "zh-Hant", "zh-Hans", "ja", "ko"],
   sourceLocale: "en",
   fallbackLocales: {
     default: "en",
